d08bf7c4e970a65e928e32500ab4e968ffd94419
ivanho-git <ibhaanm29@gmail.com>
2025-07-18 17:55:15 +0530

    Update dropdown.py

--- dropdown.py
+++ dropdown.py
@@ -1,2 +1,8 @@
 form_frame = Frame(root, bg="#2C3333")
 form_frame.pack(pady=10)
+
+Label(form_frame, text="🌆 Enter City Name:", **LABEL_STYLE).grid(row=0, column=0, padx=10)
+
+city_var = StringVar()
+city_entry = Entry(form_frame, textvariable=city_var, **ENTRY_STYLE, width=25, relief=FLAT)
+city_entry.grid(row=0, column=1, padx=10)
