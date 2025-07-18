ef0985242a873791db55052571139c57771df102
ivanho-git <ibhaanm29@gmail.com>
2025-07-18 17:57:04 +0530

    Create status bar.py

---
+++ status bar.py
@@ -0,0 +1,4 @@
+status_bar = Label(root, text="👋 Welcome to Fancy Weather App!", bd=1, relief=SUNKEN, anchor=W, bg="#222831", fg="white", font=("Poppins", 10))
+status_bar.pack(side=BOTTOM, fill=X)
+
+root.mainloop()
